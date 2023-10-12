4adc17f9e18dc2a098f7fdfe32bdcedd59ca8c0f
Desiree Zarco <desireeannzarco7@gmail.com>
2023-10-11 18:24:08 -0700

    redid lab since lost files

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
-const data = {
-    name: "Steve",
-    email: "steve@steve.com"
-}
-
-const configurationObject = {
-    method: 'POST',
-    headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-   },
-    body: JSON.stringify(data)
-}
-
-function submitData(user, email) {
-  return fetch(`http://localhost:3000/users`, configurationObject) 
-    .then(res => res.json())
-    .then((data) => renderId(data))
-    .catch((error) => renderError(error))
-}
-
-function renderId(data){
-    const div = document.createElement('div')
-    div.textContent = data.id
-    document.querySelector('body').append(div)
-}
-
-function renderError(error){
-    const div = document.createElement('div')
-    div.textContent = error.message
-    document.querySelector('body').append(div)
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          name, 
+          email
+        })
+      })
+      .then(response => response.json())
+      .then(object => {
+        document.body.innerHTML = object["id"];
+      })
+      .catch(function(error) {
+        const newTag = document.createElement("p");
+        const text = document.createTextNode(error.message);
+        newTag.appendChild(text);
+        document.body.appendChild(newTag);
+    })
 }
